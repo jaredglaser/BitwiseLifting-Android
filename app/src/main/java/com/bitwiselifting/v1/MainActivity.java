28d1352973fe5cc74f8f77e3e9fa27b01499490a
Jared Glaser <jared@glaserfamily.net>
2019-04-03 09:55:54 -0400

    added init files for orientation view

--- app/src/main/java/com/bitwiselifting/v1/MainActivity.java
+++ app/src/main/java/com/bitwiselifting/v1/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bitwiselifting.v1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         * View Declarations
+         */
+        Button orientationTesting = findViewById(R.id.orientationTestingbutton);
+
+
+
+        /*
+         * Listener Declarations
+         */
+        orientationTesting.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), Orientation.class);
+                startActivityForResult(myIntent, 0);
+
+            }
+        });
+
+
     }
 }
